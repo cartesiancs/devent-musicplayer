e9dc86384c674835d3c4b8bb7d3642b91ea3a720
Dipokal HHJ <hhj@devent.kr>
2022-07-16 13:58:01 +0900

    fix: edited db path
    + 도커 볼륨을 위한 데이터베이스 경로 수정

--- server/databases/connect.js
+++ server/databases/connect.js
@@ -6,7 +6,7 @@ import { fileURLToPath } from 'url';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-const db_dir = path.join(__dirname, "dmpdb.db");
+const db_dir = path.join(__dirname, "data/dmpdb.db");
 const conn = sqlite3.verbose()
 
 const db = new conn.Database(db_dir, async (error) => {
